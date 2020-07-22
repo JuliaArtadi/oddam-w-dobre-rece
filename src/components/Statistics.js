af9179befacd70c15e80c6150cc91df424d511f4
JuliaArtadi <julia.a.bandera@gmail.com>
2020-07-22 15:36:38 +0200

    Section statistics - web view

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,29 @@
 import React from "react";
-import {Element} from 'react-scroll';
+
 export const Statistics = () => {
     return (
-        <Element name={'info'} className={'stats'}>
-
-        </Element>
+        <section name={'info'} className={'stats'}>
+            <div className={'stats__col'}>
+                <h2 className={'stats__number'}>10</h2>
+                <p className={'stats__title'}>Oddanych worków</p>
+                <p className={'stats__text'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                </p>
+            </div>
+            <div className={'stats__col'}>
+                <h2 className={'stats__number'}>5</h2>
+                <p className={'stats__title'}>Wspartych organizacji</p>
+                <p className={'stats__text'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                </p>
+            </div>
+            <div className={'stats__col'}>
+                <h2 className={'stats__number'}>7</h2>
+                <p className={'stats__title'}>Zorganizowanych zbiórek</p>
+                <p className={'stats__text'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                </p>
+            </div>
+        </section>
     )
 }
