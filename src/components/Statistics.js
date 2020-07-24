e130fbb30469cbe1505c347926b6a40192f3aa83
JuliaArtadi <julia.a.bandera@gmail.com>
2020-07-24 11:22:07 +0200

    Section Contact - web view

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,23 +7,27 @@ export const Statistics = () => {
                 <h2 className={'stats__number'}>10</h2>
                 <p className={'stats__title'}>Oddanych worków</p>
                 <p className={'stats__text'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
                 </p>
             </div>
             <div className={'stats__col'}>
                 <h2 className={'stats__number'}>5</h2>
                 <p className={'stats__title'}>Wspartych organizacji</p>
                 <p className={'stats__text'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
                 </p>
             </div>
             <div className={'stats__col'}>
                 <h2 className={'stats__number'}>7</h2>
                 <p className={'stats__title'}>Zorganizowanych zbiórek</p>
                 <p className={'stats__text'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                    Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
                 </p>
             </div>
         </section>
+
     )
 }
